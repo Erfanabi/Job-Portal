7a9005280d1b093874ee29f7673fc96c952651fe
erfan sharafi <erfansharafi60@gmail.com>
2025-07-19 20:30:05 +0330

    Refactor App structure and routing
    - Removed existing CSS styles from App.css.
    - Updated App.jsx to implement React Router with routes for Home, ApplyJob, and Applications pages.
    - Wrapped App component with BrowserRouter and AppContextProvider in main.jsx for routing and context management.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ApplyJob from "./pages/ApplyJob";
+import Applications from "./pages/Applications";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>salam</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aplly-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Applications />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const value = {};
+
+  return <AppContext.Provider>{props.children}</AppContext.Provider>;
+};
