b7b6a89b8e605b31bf046ee796274ba2a30278f0
erfan sharafi <erfansharafi60@gmail.com>
2025-08-01 10:41:49 +0330

    feat(Dashboard): enhance navigation and logout functionality
    - Added useEffect to navigate to the manage jobs page if company data is available.
    - Updated context usage to include setCompanyData and setCompanyToken for logout functionality.
    - Implemented logout logic to clear local storage and reset context state upon user logout.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
@@ -6,7 +6,12 @@ import { AppContext } from "../context/AppContext";
 function Dashboard() {
   const navigate = useNavigate();
 
-  const { companyData } = useContext(AppContext);
+  const { companyData, setCompanyData, setCompanyToken } =
+    useContext(AppContext);
+
+  useEffect(() => {
+    if (companyData) navigate("/dashboard/manage-jobs");
+  }, [companyData]);
 
   return (
     <div className="min-h-screen">
@@ -33,7 +38,17 @@ function Dashboard() {
 
               <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
                 <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm">
-                  <li className="py-1 px-2 cursor-pointer pr-10">Logout</li>
+                  <li
+                    className="py-1 px-2 cursor-pointer pr-10"
+                    onClick={() => {
+                      localStorage.removeItem("companyToken");
+                      setCompanyData(null);
+                      setCompanyToken(null);
+                      navigate("/");
+                    }}
+                  >
+                    Logout
+                  </li>
                 </ul>
               </div>
             </div>
